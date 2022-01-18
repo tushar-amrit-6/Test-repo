0f7f955d5b7f8390faf98e315f31b411f40e45af
tushar-amrit-6 <tusharpubg02.com>
2022-01-18 11:07:46 +0530

    first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author Tushar
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first project");
+	}
+
+}
